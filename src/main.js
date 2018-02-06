bf3e4c1cd5107313905943b780264606eaa223ca
Gabrielle von Koss <gabivk@gmail.com>
2018-02-06 12:09:06 +0000

    initialize js

--- src/main.js
+++ src/main.js
@@ -3,6 +3,4 @@
 require('o-header-services');
 require('o-table');
 
-document.addEventListener('DOMContentLoaded', function() {
-	document.dispatchEvent(new CustomEvent('o.DOMContentLoaded'));
-});
+require('o-autoinit');
