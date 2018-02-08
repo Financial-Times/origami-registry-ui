f4e43eaf6257a6919a59e6e4bba86873c10c0a1d
notlee <lee.moody@ft.com>
2018-02-08 17:38:26 +0000

    linting

--- src/js/demo.js
+++ src/js/demo.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = (function(){
-    window.addEventListener("message", (e) => {
+    window.addEventListener('message', (e) => {
         const data = JSON.parse(e.data);
         if (data.type === 'resize' && data.url && data.height) {
             const iframesToResize = document.querySelectorAll(`iframe[src="${data.url}"]`);
