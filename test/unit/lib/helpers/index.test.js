bee1c7700be2a2acfe2e622e2aeabf221f1d85dd
Rowan Manning <accounts+github@rowanmanning.com>
2018-02-23 11:49:36 +0000

    Add a filter form to the overview page
    Currently this only works server-side, but a lot of the logic is
    shareable with the client side. Also the styling isn't quite there yet,
    but I wanted to get all the functionality working first.

--- test/unit/lib/helpers/index.test.js
+++ test/unit/lib/helpers/index.test.js
@@ -1,11 +1,14 @@
 'use strict';
 
 const assert = require('proclaim');
-// const sinon = require('sinon');
-
-const helper = require('../../../../lib/helpers');
 
 describe('helpers', () => {
+	let helper;
+
+	beforeEach(() => {
+		helper = require('../../../../lib/helpers');
+	});
+
 	it('capitalises a string', () => {
 		const string = 'lowercase';
 		assert.strictEqual(helper.capitalise(string), 'Lowercase');
