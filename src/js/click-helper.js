d3737a9b3f6b1cf71b731acec44bd94ceca0a851
Gabrielle <gabivk@gmail.com>
2018-02-26 12:04:59 +0000

    fix click helper

--- src/js/click-helper.js
+++ src/js/click-helper.js
@@ -5,7 +5,14 @@ module.exports = (function() {
 	Array.from(links, link => {
 		const row = link.parentNode.parentNode;
 		if (link.href) {
-			row.addEventListener('click', () => { location.href = link.href; });
+			row.addEventListener('click', (e) => { 
+				e.preventDefault();
+				if (e.metaKey || e.ctrlKey || e.which === 2) {
+					window.open(link.href, '_blank');
+				} else {
+					location.href = link.href; 
+				}
+			});
 		}
 	});
 }());
