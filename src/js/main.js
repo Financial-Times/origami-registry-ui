59dd4083ed4698234a989d2043f942842789b3ca
Gabrielle von Koss <gabivk@gmail.com>
2018-02-28 16:36:49 +0000

    Surface html (#58)
    * fetch plain html
    
    * add highlight.js
    
    * add stynax highlighting styles
    
    * add more styling to hljs classes as and when required
    
    * dont need a whole file for highlighting actually
    
    * show product name
    
    * remove extra lib file
    
    * updates promises, and origami-service handlebars config solves tabs

--- src/js/main.js
+++ src/js/main.js
@@ -3,13 +3,16 @@
 require('./click-helper');
 require('./demo');
 require('./versioning');
+
 const ComponentListing = require('./component-listing');
 const FilterForm = require('./filter-form');
+const hljs = require('highlight.js');
 
 // Initialise components
 document.addEventListener('o.DOMContentLoaded', () => {
 	ComponentListing.init();
 	FilterForm.init();
+	hljs.initHighlighting();
 });
 
 // Dispatch the o.DOMContentLoaded event
