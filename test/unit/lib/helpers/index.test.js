325a4860f0ff58d66915d1f63898255c6df63a7b
Gabrielle von Koss <gabivk@gmail.com>
2018-03-23 12:36:24 +0000

    add helper tests

--- test/unit/lib/helpers/index.test.js
+++ test/unit/lib/helpers/index.test.js
@@ -9,8 +9,74 @@ describe('helpers', () => {
 		helper = require('../../../../lib/helpers');
 	});
 
-	it('capitalises a string', () => {
+	it('.capitalise', () => {
 		const string = 'lowercase';
 		assert.strictEqual(helper.capitalise(string), 'Lowercase');
+	});
+
+	it('.slugify', () => {
+		const string = 'lower case';
+		assert.strictEqual(helper.slugify(string), 'lower-case');
+	});
+
+	it('.json', () => {
+		const string = {string: 'lowercase'};
+		assert.strictEqual(helper.json(string), '{"string":"lowercase"}');
+	});
+
+	describe('conditional helpers', () => {
+		let args = [];
+		const opts = {
+			fn: () => true,
+			inverse: () => false
+		};
+
+		describe('.ifEquals', () => {
+			it('confirms two arguments are equal (===)', () => {
+				args = ['string', 'string'];
+				assert.strictEqual(helper.ifEquals(args[0], args[1], opts), true);
+			});
+
+			it('confirms two arguments are _not_ equal (!==)', () => {
+				args = ['string', ''];
+				assert.strictEqual(helper.ifEquals(args[0], args[1], opts), false);
+			});
+		});
+
+		describe('.ifBoth', () => {
+			it('confirms two arguments are both present (&&)', () => {
+				args = ['string', 'string'];
+				assert.strictEqual(helper.ifBoth(args[0], args[1], opts), true);
+			});
+
+			it('confirms two arguments are _not_ both present', () => {
+				args = ['string'];
+				assert.strictEqual(helper.ifBoth(args[0], args[1], opts), false);
+			});
+		});
+
+		describe('.ifEither', () => {
+			it('confirms one of two arguments is present (||)', () => {
+				args = ['string'];
+				assert.strictEqual(helper.ifEither(args[0], args[1], opts), true);
+			});
+
+			it('confirms neither of two arguments are present', () => {
+				args = [];
+				assert.strictEqual(helper.ifEither(args[0], args[1], opts), false);
+			});
+		});
+
+		describe('.unlessEquals', () => {
+			it('cconfirms two arguments are _not_ equal (!==)', () => {
+				args = ['string', ''];
+				assert.strictEqual(helper.unlessEquals(args[0], args[1], opts), true);
+			});
+
+			it('confirms two arguments are equal ', () => {
+				args = ['string', 'string'];
+				assert.strictEqual(helper.unlessEquals(args[0], args[1], opts), false);
+			});
+		});
 	});
 });
