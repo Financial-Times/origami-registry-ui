c04e06c7d02a91d30001ae503d36b91774e0c18e
notlee <lee.moody@ft.com>
2018-03-28 11:54:16 +0100

    Color contrast -- and a little BEM

--- src/js/click-helper.js
+++ src/js/click-helper.js
@@ -1,16 +1,16 @@
 'use strict';
 
 module.exports = (function() {
-	const links = document.querySelectorAll('.registry__table-cell--link');
+	const links = document.querySelectorAll('.registry__table-cell__link');
 	Array.from(links, link => {
 		const row = link.parentNode.parentNode;
 		if (link.href) {
-			row.addEventListener('click', (e) => { 
+			row.addEventListener('click', (e) => {
 				e.preventDefault();
 				if (e.metaKey || e.ctrlKey || e.which === 2) {
 					window.open(link.href, '_blank');
 				} else {
-					location.href = link.href; 
+					location.href = link.href;
 				}
 			});
 		}
