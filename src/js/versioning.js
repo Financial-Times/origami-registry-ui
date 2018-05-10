5a9b712700b2f35930ed728e95ad9746b90d5a49
Gabrielle von Koss <gabivk@gmail.com>
2018-05-10 15:00:21 +0100

    update version selection to maintain brand (if applicable)

--- src/js/versioning.js
+++ src/js/versioning.js
@@ -3,6 +3,8 @@
 module.exports = (function () {
 	const selection = document.querySelector('.version__select');
 	if (selection) {
-		selection.addEventListener('change', () => { location.href = selection.value; });
+		selection.addEventListener('change', () => {
+			location.href = selection.value + location.search;
+		});
 	}
 }());
