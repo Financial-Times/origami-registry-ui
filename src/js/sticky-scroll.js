daf8d813216f15484e630d505233f0f397375ea4
Gabrielle von Koss <gabivk@gmail.com>
2018-05-11 16:45:02 +0100

    fix sticky scroll to stop at footer

--- src/js/sticky-scroll.js
+++ src/js/sticky-scroll.js
@@ -3,16 +3,20 @@
 module.exports = (function() {
 	const wrappers = document.querySelectorAll('.registry__container--wrapper');
 	const header = document.querySelector('header');
+	const footer = document.querySelector('footer');
 
 	document.addEventListener('scroll', () => {
 		Array.from(wrappers, wrapper => {
 			if (wrapper && wrapper.offsetHeight >= window.innerHeight) {
-				if (header.offsetHeight <= window.scrollY) {
+				if ((wrapper.offsetHeight + window.scrollY) > footer.offsetTop) {
+					wrapper.classList.remove('registry__container--sticky');
+				} else if (header.offsetHeight <= window.scrollY) {
 					wrapper.classList.add('registry__container--sticky');
 				} else {
 					wrapper.classList.remove('registry__container--sticky');
 				}
 			}
+
 		});
 	});
 }());
