c15342d1c4c5ed4109dc9ba2078a6fc85d11b551
Gabrielle von Koss <gabivk@gmail.com>
2018-05-15 10:07:17 +0100

    New layout fixes (#101)
    * fix layout bug on demo tabpanels
    
    * add link to brand message
    
    * update message markup
    
    * update message logic
    
    * update version selection to maintain brand (if applicable)
    
    * vary brand message based on version
    
    * fix sticky bug
    
    * clarofy rows in table
    
    * remove oTable/table - add list
    
    * update specificity for hidden list items
    
    * allow both sidebars to be sticky on overview page
    
    * add aria roles
    
    * update tests
    
    * fix sticky scroll to stop at footer
    
    * bump iframe loading speed - link preload
    
    * fix janky stickyness

--- src/js/sticky-scroll.js
+++ src/js/sticky-scroll.js
@@ -3,13 +3,20 @@
 module.exports = (function() {
 	const wrappers = document.querySelectorAll('.registry__container--wrapper');
 	const header = document.querySelector('header');
+	const footer = document.querySelector('footer');
 
 	document.addEventListener('scroll', () => {
 		Array.from(wrappers, wrapper => {
-			if (wrapper && wrapper.offsetHeight > window.innerHeight) {
-				wrapper.style.overflowY = 'scroll';
-				wrapper.style.position = (header.offsetHeight <= window.scrollY) ? 'fixed' : 'relative';
+			if (wrapper && wrapper.offsetHeight >= window.innerHeight) {
+				if ((wrapper.offsetHeight + window.scrollY) > footer.offsetTop) {
+					wrapper.classList.remove('registry__container--sticky');
+				} else if (header.offsetHeight <= window.scrollY) {
+					wrapper.classList.add('registry__container--sticky');
+				} else {
+					wrapper.classList.remove('registry__container--sticky');
+				}
 			}
+
 		});
 	});
 }());
