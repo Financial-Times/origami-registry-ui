e44f1e8c9aaeb70371da227fe8cb85da13be1979
notlee <lee.moody@ft.com>
2018-08-02 14:00:56 +0100

    Readme with defined and generated nav

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,15 @@
 'use strict';
 
 require('o-syntax-highlight');
+require('prism/components/prism-diff.js');
 require('prism/components/prism-scss.js');
+require('prism/components/prism-css.js');
 require('./click-helper');
 require('./demo');
 require('./select-text');
 require('./sticky-scroll');
 require('./versioning');
+require('./readme');
 
 const ComponentListing = require('./component-listing');
 const FilterForm = require('./filter-form');
