66a3589372bea3b3df5ed513452bbf6a21ebd665
notlee <lee.moody@ft.com>
2018-08-03 10:36:42 +0100

    Add filterable subnavs

--- src/js/main.js
+++ src/js/main.js
@@ -11,12 +11,14 @@ require('./sticky-scroll');
 require('./versioning');
 require('./readme');
 
+const FilterableNav = require('./filterable-nav');
 const ComponentListing = require('./component-listing');
 const FilterForm = require('./filter-form');
 const LinkedHeading = require('./linked-heading');
 
 // Initialise components
 document.addEventListener('o.DOMContentLoaded', () => {
+	FilterableNav.init();
 	ComponentListing.init();
 	FilterForm.init();
 	LinkedHeading.init();
