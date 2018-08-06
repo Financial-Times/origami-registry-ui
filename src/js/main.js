acf7f7789afac2124ac4218c1f02d144713066c5
notlee <lee.moody@ft.com>
2018-08-06 15:59:57 +0100

    bump o-syntax

--- src/js/main.js
+++ src/js/main.js
@@ -1,15 +1,11 @@
 'use strict';
 
 require('o-syntax-highlight');
-require('prism/components/prism-diff.js');
-require('prism/components/prism-scss.js');
-require('prism/components/prism-css.js');
 require('./click-helper');
 require('./demo');
 require('./select-text');
 require('./sticky-scroll');
 require('./versioning');
-require('./readme');
 
 const FilterableNav = require('./filterable-nav');
 const ComponentListing = require('./component-listing');
