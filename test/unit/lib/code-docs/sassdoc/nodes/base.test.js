650c6ab3bb56606018f1f24d90b86a59f999ea81
notlee <lee.moody@ft.com>
2018-08-09 11:30:00 +0100

    sassdoc base unit test

--- test/unit/lib/code-docs/sassdoc/nodes/base.test.js
+++ test/unit/lib/code-docs/sassdoc/nodes/base.test.js
@@ -63,9 +63,38 @@ describe('lib/code-docs/sassdoc/nodes/base', () => {
                 'description': 'The number to double'
             }
         ],
+        'aliased': [
+            'oTestComponentDoubler'
+        ]
     };
 
-    it('creates a node to represent a SassDoc doclet with basic properties', () => {
+    const simpleDoclet = {
+        'description': '',
+        'commentRange': {
+            'start': 17,
+            'end': 23
+        },
+        'context': {
+            'type': 'function',
+            'name': 'oTestComponentDouble',
+            'code': '\n    @return 2 * $scale;\n',
+            'line': {
+                'start': 24,
+                'end': 26
+            }
+        },
+        'group': [
+            undefined
+        ],
+        'access': 'public',
+        'require': [],
+        'file': {
+            'path': 'src/scss/_functions.scss',
+            'name': '_functions.scss'
+        }
+    };
+
+    it('creates a node to represent a complex SassDoc doclet with basic properties', () => {
         const doclet = comprehensiveDoclet;
         const node = new SassDocBaseNode(doclet);
         assert.equal(node.name, 'oTestComponentDouble', 'Did not add expected name property.');
@@ -97,24 +126,96 @@ describe('lib/code-docs/sassdoc/nodes/base', () => {
         ], 'Did not add expected examples property.');
     });
 
-    describe('addAliases', () => {
+    it('creates a node to represent a simple SassDoc doclet with basic properties', () => {
+        const doclet = simpleDoclet;
+        const node = new SassDocBaseNode(doclet);
+        assert.equal(node.name, 'oTestComponentDouble', 'Did not add expected name property.');
+        assert.equal(node.description, '', 'Did not add expected description property.');
+        assert.equal(node.longname, 'function-oTestComponentDouble', 'Did not add expected longname property.');
+        assert.deepEqual(node.file, {
+            'path': 'src/scss/_functions.scss',
+            'name': '_functions.scss',
+            'lineno': 24
+        }, 'Did not add expected file property.');
+        assert.deepEqual(node.group, {
+            key: '',
+            name: ''
+        }, 'Did not add expected group property.');
+        assert.deepEqual(node.links, [], 'Expect an empty links array.');
+        assert.deepEqual(node.examples, [], 'Expect an empty examples array.');
+        assert.equal(node.brand, undefined, 'Did not expect a brand property on the simple SassDoc example.');
+        assert.equal(node.deprecated, undefined, 'Did not expect a deprecation notice on the simple SassDoc example.');
+    });
 
+    it('if provided the groupName property added by SassDoc extras is used for the node group property', () => {
+        // @see https://github.com/SassDoc/sassdoc-extras/blob/2.4.3/src/groupName.js#L30
+        const simpleExampleDoclet = simpleDoclet;
+        simpleExampleDoclet.groupName = {
+            // group : label
+            undefined: 'o-example'
+        };
+        const simpleNode = new SassDocBaseNode(simpleExampleDoclet);
+        assert.deepEqual(simpleNode.group, {
+            key: '',
+            name: 'o-example'
+        });
+
+        const comprehensiveExampleDoclet = comprehensiveDoclet;
+        comprehensiveExampleDoclet.groupName = {
+            // group : label
+            'helpers': 'Utils'
+        };
+        const comprehensiveNode = new SassDocBaseNode(comprehensiveExampleDoclet);
+        assert.deepEqual(comprehensiveNode.group, {
+            key: 'helpers',
+            name: 'Utils'
+        });
+    });
+
+    describe('addAliases', () => {
+        it('Adds aliases to the node', () => {
+            const doclet = comprehensiveDoclet;
+            const node = new SassDocBaseNode(doclet);
+            node.addAliases(doclet);
+            assert.deepEqual(node.aliases, ['oTestComponentDoubler'], 'Did not add expected aliases property.');
+        });
+
+        it('Adds an empty aliases array for doclets which have no aliases', () => {
+            const doclet = simpleDoclet;
+            const node = new SassDocBaseNode(doclet);
+            node.addAliases(doclet);
+            assert.deepEqual(node.aliases, []);
+        });
     });
 
     describe('addParameters', () => {
-        const doclet = comprehensiveDoclet;
-        const node = new SassDocBaseNode(doclet);
-        node.addParameters(doclet);
-        assert.deepEqual(node.parameters, [{
-            name: 'toDouble',
-            type: ['Number'],
-            description: 'The number to double',
-            default: '',
-            optional: false
-        }], 'Did not add expected parameters property.');
+        it('Adds parameters to the node', () => {
+            const doclet = comprehensiveDoclet;
+            const node = new SassDocBaseNode(doclet);
+            node.addParameters(doclet);
+            assert.deepEqual(node.parameters, [{
+                name: 'toDouble',
+                type: ['Number'],
+                description: 'The number to double',
+                default: '',
+                optional: false
+            }], 'Did not add expected parameters property.');
+        });
+
+        it('Adds an empty parameters array for doclets which have no parameters', () => {
+            const doclet = simpleDoclet;
+            const node = new SassDocBaseNode(doclet);
+            node.addParameters(doclet);
+            assert.deepEqual(node.parameters, []);
+        });
     });
 
     describe('parseTypes', () => {
-
+        it('Transform a string of SassDoc types to an array.', () => {
+            const doclet = comprehensiveDoclet;
+            const node = new SassDocBaseNode(doclet);
+            assert.deepEqual(node.parseTypes('Color | Null'), ['Color', 'Null']);
+            assert.deepEqual(node.parseTypes(''), []);
+        });
     });
 });
