aa2a95cff734a609db3b50e722e9640b508ecd7b
notlee <lee.moody@ft.com>
2018-08-09 17:18:42 +0100

    Test sassdoc getNodesByKind

--- test/unit/lib/code-docs/sassdoc/index.test.js
+++ test/unit/lib/code-docs/sassdoc/index.test.js
@@ -2,6 +2,12 @@
 
 const assert = require('proclaim');
 const SassDoc = require('../../../../../lib/code-docs/sassdoc');
+const VariableNode = require('../../../../../lib/code-docs/sassdoc/nodes/variable');
+const MixinNode = require('../../../../../lib/code-docs/sassdoc/nodes/mixin');
+const FunctionNode = require('../../../../../lib/code-docs/sassdoc/nodes/function');
+const MixinDoclet = require('../../../mock/code-docs/sassdoc/mixin');
+const FunctionDoclet = require('../../../mock/code-docs/sassdoc/function');
+const VariableDoclet = require('../../../mock/code-docs/sassdoc/variable');
 
 describe('lib/code-docs/sassdoc/index', () => {
 
@@ -9,6 +15,50 @@ describe('lib/code-docs/sassdoc/index', () => {
         it('Returns an array of supported doclet kinds', () => {
             assert.isTrue(Array.isArray(SassDoc.supportedDoclets()), 'Did not return an array.');
             assert.isTrue(SassDoc.supportedDoclets().includes('mixin'), 'Expected at least mixin doclets to be supported.');
+        });
+    });
+
+    describe('getNodesByKind', () => {
+        const testSassDoc = new SassDoc('o-test-component', 'master', [
+            MixinDoclet.comprehensiveDoclet,
+            FunctionDoclet.comprehensiveDoclet,
+            VariableDoclet.comprehensiveDoclet
+        ]);
+        const nodes = testSassDoc.getNodesByKind();
+        assert.isTrue(nodes.mixin[0] instanceof MixinNode, 'Did not return an object with a mixins property containing the formatted mixin node.');
+        assert.isTrue(nodes.function[0] instanceof FunctionNode, 'Did not return an object with a functions property containing the formatted function node.');
+        assert.isTrue(nodes.variable[0] instanceof VariableNode, 'Did not return an object with a variables property containing the formatted variable node.');
+    });
+
+    describe('formatDoclet', () => {
+        it('formats a function doclet', () => {
+            const doclet = FunctionDoclet.comprehensiveDoclet;
+            const node = SassDoc.formatDoclet(doclet);
+            assert.isTrue(node instanceof FunctionNode, 'Did not create a function node from a function doclet.');
+        });
+        it('formats a mixin doclet', () => {
+            const doclet = MixinDoclet.comprehensiveDoclet;
+            const node = SassDoc.formatDoclet(doclet);
+            assert.isTrue(node instanceof MixinNode, 'Did not create a mixin node from a mixin doclet.');
+        });
+        it('formats a variable doclet', () => {
+            const doclet = VariableDoclet.comprehensiveDoclet;
+            const node = SassDoc.formatDoclet(doclet);
+            assert.isTrue(node instanceof VariableNode, 'Did not create a variable node from a variable doclet.');
+        });
+        it('throws an error for a placheolder doclet', () => {
+            const doclet = {
+                kind: 'placeholder'
+            };
+            const formatDoclet = SassDoc.formatDoclet.bind(null, doclet);
+            assert.throws(formatDoclet, null, 'Should throw an error for an placeholder doclet kind.');
+        });
+        it('throws an error for an unsupported doclet', () => {
+            const doclet = {
+                kind: 'somethingunknown'
+            };
+            const formatDoclet = SassDoc.formatDoclet.bind(null, doclet);
+            assert.throws(formatDoclet, null, 'Should throw an error for an unsupported doclet kind.');
         });
     });
 
@@ -24,7 +74,9 @@ describe('lib/code-docs/sassdoc/index', () => {
                     'type': 'function',
                     'name': 'oTestComponentDouble',
                 },
-                'group': [],
+                'group': [
+                    'undefined'
+                ],
                 'access': 'public'
             };
         });
@@ -51,6 +103,14 @@ describe('lib/code-docs/sassdoc/index', () => {
         });
         it('does not remove supported, public doclets which are for the current brand', () => {
             assert.ok(new SassDoc(componentName, brand, [doclet]).getNodes()[0]);
+        });
+        it('uses the component name instead of an "undefined" doclet group', () => {
+            assert.equal(new SassDoc(componentName, brand, [doclet]).getNodes()[0].group.name, componentName);
+        });
+        it('returns array with formatted doclets', () => {
+            const nodes = new SassDoc(componentName, brand, [doclet]).getNodes();
+            assert.isTrue(Array.isArray(nodes));
+            assert.isTrue(nodes[0] instanceof FunctionNode);
         });
     });
 });
