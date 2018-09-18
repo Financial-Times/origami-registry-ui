d950c7b8cbd2fbb4eeb2fb375eaa105820dd640f
Carles Andres <carlos.andres.c@gmail.com>
2018-09-18 11:50:16 +0100

    Added defensive code around fragile method calls

--- src/js/demo.js
+++ src/js/demo.js
@@ -2,12 +2,16 @@
 
 module.exports = (function(){
 	window.addEventListener('message', (e) => {
-		const data = JSON.parse(e.data);
-		if (data.type === 'resize' && data.url && data.height) {
-			const iframesToResize = document.querySelectorAll(`iframe[src="${data.url}"]`);
-			iframesToResize.forEach((iframe) => {
-				iframe.height = data.height;
-			});
+		try {
+			const data = JSON.parse(e.data);
+			if (data.type === 'resize' && data.url && data.height) {
+				const iframesToResize = document.querySelectorAll(`iframe[src="${data.url}"]`);
+				iframesToResize.forEach((iframe) => {
+					iframe.height = data.height;
+				});
+			}
+		} catch(e) {
+			return;
 		}
 	}, false);
 }());
