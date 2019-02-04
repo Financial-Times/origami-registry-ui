2ebbca3871f5803b4f17aa29d97c0ccfbbd22c7f
notlee <lee.moody@ft.com>
2019-02-04 11:11:41 +0000

    Show correct brand/version when page is cached (on the back/forward button).

--- src/js/versioning.js
+++ src/js/versioning.js
@@ -4,7 +4,13 @@ module.exports = {
 	init: function () {
 		const selections = document.querySelectorAll('[data-version-select]');
 		selections.forEach(selection => selection.addEventListener('change', (event) => {
-			location.href = event.target.selectedOptions.item(0).getAttribute('data-redirect');
+			// Get a redirect location from the selected option.
+			const newLoaction = event.target.selectedOptions.item(0).getAttribute('data-redirect');
+			// The selection is cached by the browser, which is misleading when the back/forward
+			// buttons are hit. So reset the selection to what it was before redirecting.
+			selection.querySelector('[selected="true"]').selected = true;
+			// Go to the selected page.
+			location.href = newLoaction;
 		}));
 	}
 };
