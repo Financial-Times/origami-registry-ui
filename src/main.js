1c3fcfd0ab57fc37bcfb358b779ed9c4e0c11ba9
notlee <lee.moody@ft.com>
2020-04-03 18:18:13 +0100

    Upgrade o-table

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import 'o-header-services';
 import 'o-message';
 import 'o-syntax-highlight';
 import 'o-tabs';
+import 'o-table';
 
 require('./js/main');
-require('o-table');
 require('o-autoinit');
