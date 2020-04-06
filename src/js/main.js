525bf55789d09a59ea108cbbaa80b20192e569b8
notlee <lee.moody@ft.com>
2020-04-06 17:17:51 +0100

    Nasty commit: upgrade o-forms, fix o-tabs use, fix header logo

--- src/js/main.js
+++ src/js/main.js
@@ -1,19 +1,16 @@
 'use strict';
 
-require('o-layout');
-require('./demo');
-
-const SelectText = require('./select-text');
-const Versioning = require('./versioning');
-const FilterableNav = require('./filterable-nav');
-const ComponentListing = require('./component-listing');
-const FilterForm = require('./filter-form');
+import SelectText from './select-text';
+import Versioning from './versioning';
+import FilterableNav from './filterable-nav';
+import ComponentListing from './component-listing';
+import FilterForm from './filter-form';
 
 // Initialise components
 document.addEventListener('o.DOMContentLoaded', () => {
-	FilterableNav.init();
-	ComponentListing.init();
-	FilterForm.init();
-	Versioning.init();
-	SelectText.init();
+    FilterableNav.init();
+    ComponentListing.init();
+    FilterForm.init();
+    Versioning.init();
+    SelectText.init();
 });
--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import 'o-message';
 import 'o-syntax-highlight';
 import 'o-tabs';
 import 'o-table';
+import 'o-layout';
+import './js/demo';
+import './js/main';
 import 'o-autoinit';
-
-require('./js/main');
