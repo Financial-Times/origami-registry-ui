ad61f022727e524342ac093e8c11df98d78f6e34
Jake Champion <me@jakechampion.name>
2020-09-04 15:59:00 +0100

    Start tracking website metrics using o-tracking v3

--- src/main.js
+++ src/main.js
@@ -1,5 +1,4 @@
 
-
 import 'o-header-services';
 import 'o-message';
 import 'o-syntax-highlight';
@@ -9,3 +8,32 @@ import 'o-layout';
 import './js/demo';
 import './js/main';
 import 'o-autoinit';
+import oTracking from 'o-tracking';
+import CookieMessage from 'o-cookie-message';
+
+const cookieMessage = new CookieMessage();
+
+const hasConsentedToCookies = cookieMessage.shouldShowCookieMessage() === false;
+if (hasConsentedToCookies) {
+	turnOnTracking();
+} else {
+	document.body.addEventListener('oCookieMessage.act', () => {
+		turnOnTracking();
+	});
+}
+
+function turnOnTracking() {
+	oTracking.init({
+		context: {
+			product: 'origami-registry-ui',
+		}
+	});
+	// Send a page view tracking event.
+	oTracking.page();
+	// Track click events on the page.
+	oTracking.click.init();
+	// Track when elements with the attribute `data-o-tracking-view` are visible to the user.
+	oTracking.view.init();
+	// Tell o-tracking to listen for custom events named `oTracking.event`.
+	oTracking.event.init();
+}
