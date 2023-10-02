a7912e23776e7813a6bf02b0feba0b5609fbf813
Lee Moody <lee.moody@ft.com>
2023-10-02 15:23:27 +0100

    404 for any Origami component the Registry does not support explicitly.
    We are very unlikely to present "O3" Origami components, components
    with an `origamiVersion` set to `3.0` in the registry. Instead we
    are using Storybook and intend to decommission the registry.
    
    We also intend to decommission the Registry's source of data,
    origami-repo-data, which will not ingest "O3" components.
    
    In theory then, this change is not required. It's a little defensive.
    However, if we change our minds on repo-data, or something goes wrong,
    then the Registry should not behave in an unexpected way.
    
    Note that this also fixes a bug related to `origamiVersion`. Repo
    Data always returns a string but here we were assuming an integer.
    We relied on mocks for our tests, which made the same incorrect
    assumption.

--- test/integration/mock/repo-data-api/data/repos.js
+++ test/integration/mock/repo-data-api/data/repos.js
@@ -8,7 +8,7 @@ module.exports = [
 		name: 'o-example-active',
 		type: 'module',
 		subType: 'primitives',
-		origamiVersion: 1,
+		origamiVersion: '1',
 		version: '2.0.0',
 		support: {
 			status: 'active',
@@ -102,7 +102,7 @@ module.exports = [
 		name: 'o-example-no-readme',
 		type: 'module',
 		subType: 'primitives',
-		origamiVersion: 1,
+		origamiVersion: '1',
 		version: '2.0.0',
 		support: {
 			status: 'active',
@@ -149,7 +149,7 @@ module.exports = [
 		name: 'o-example-maintained',
 		type: 'module',
 		subType: null,
-		origamiVersion: 1,
+		origamiVersion: '1',
 		version: '1.5.0',
 		support: {
 			status: 'maintained',
@@ -173,7 +173,7 @@ module.exports = [
 		name: 'o-example-service-maintained',
 		type: 'service',
 		subType: null,
-		origamiVersion: 1,
+		origamiVersion: '1',
 		version: '1.5.0',
 		support: {
 			status: 'maintained',
@@ -197,7 +197,7 @@ module.exports = [
 		name: 'o-example-deprecated',
 		type: null,
 		subType: null,
-		origamiVersion: 1,
+		origamiVersion: '1',
 		version: '1.0.0',
 		support: {
 			status: 'deprecated',
@@ -221,7 +221,7 @@ module.exports = [
 		name: 'n-example-active',
 		type: null,
 		subType: null,
-		origamiVersion: 1,
+		origamiVersion: '1',
 		version: '1.2.3',
 		support: {
 			status: 'active',
@@ -245,7 +245,7 @@ module.exports = [
 		name: 'o-example-demos-except-whitelabel',
 		type: null,
 		subType: null,
-		origamiVersion: 1,
+		origamiVersion: '1',
 		version: '1.5.0',
 		support: {
 			status: 'maintained',
@@ -272,6 +272,54 @@ module.exports = [
 
 		// mock use only
 		_versions: ['1.5.0', '1.4.0', '1.3.0', '1.2.0', '1.1.0', '1.0.0']
-	}
+	},
+
+	// v3 Origami component
+	{
+		name: 'o3-example-active',
+		type: 'module',
+		subType: null,
+		origamiVersion: '3.0',
+		version: '2.0.0',
+		support: {
+			status: 'active',
+			email: 'origami@example.com',
+			channel: {
+				name: '#example-channel',
+				url: 'mock-channel-url'
+			},
+			isOrigami: true
+		},
+		resources: {
+			demos: null,
+			dependencies: [
+				{
+					'name': 'example-dependency',
+					'version': '^1.2.3',
+					'source': 'bower',
+					'isDev': false,
+					'isOptional': false
+				}
+			]
+		},
+		manifests: {
+			'origami': {
+				'browserFeatures': {
+					'required': [
+						'DOMTokenList'
+					],
+					'optional': [
+						'IntersectionObserver'
+					]
+				}
+			}
+		},
+		markdown: {
+			readme: '# o-example-active\n## test-readme\nExample content.'
+		},
+
+		// mock use only
+		_versions: ['2.0.0', '1.1.1', '1.1.0', '1.0.1', '1.0.0']
+	},
 
 ];
--- test/integration/routes/components-(id).test.js
+++ test/integration/routes/components-(id).test.js
@@ -339,4 +339,33 @@ describe('GET /components/:componentId', () => {
 
 	});
 
+	describe('when the named component version exists as an "O3" component, which the registry does not support', () => {
+
+		beforeEach(async () => {
+			request = agent.get('/components/o3-example-active@2.0.0');
+		});
+
+		it('responds with a 404 status', () => {
+			return request.expect(404);
+		});
+
+		it('responds with HTML', () => {
+			return request.expect('Content-Type', /text\/html/);
+		});
+
+		describe('HTML response', () => {
+			let html;
+
+			beforeEach(async () => {
+				html = (await request.then()).text;
+			});
+
+			it('contains the error details', () => {
+				assert.match(html, /not found/i);
+			});
+
+		});
+
+	});
+
 });
