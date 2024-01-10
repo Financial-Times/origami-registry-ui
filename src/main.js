58d93d2e476d7311f5c07f0fdab407583c80f3d9
Rhys Evans <rhys.evans@ft.com>
2024-01-10 13:51:45 +0000

    use new product and app for EE tracking

--- src/main.js
+++ src/main.js
@@ -11,6 +11,14 @@ import 'o-autoinit';
 import oTracking from 'o-tracking';
 import CookieMessage from 'o-cookie-message';
 
+
+
+function getCookie(name) {
+  const value = `; ${document.cookie}`;
+  const parts = value.split(`; ${name}=`);
+  if (parts.length === 2) return parts.pop().split(';').shift();
+}
+
 const cookieMessage = new CookieMessage();
 
 const hasConsentedToCookies = cookieMessage.shouldShowCookieMessage() === false;
@@ -23,17 +31,33 @@ if (hasConsentedToCookies) {
 }
 
 function turnOnTracking() {
-	oTracking.init({
-		context: {
-			product: 'origami-registry-ui',
-		}
-	});
-	// Send a page view tracking event.
-	oTracking.page();
-	// Track click events on the page.
-	oTracking.click.init();
-	// Track when elements with the attribute `data-o-tracking-view` are visible to the user.
-	oTracking.view.init();
-	// Tell o-tracking to listen for custom events named `oTracking.event`.
-	oTracking.event.init();
+	if (window.location.href.startsWith('https://registry.origami.ft.com/')) {
+		const oktaId = getCookie('oktaId');
+		oTracking.init({
+			context: {
+				product: 'engineering-enablement',
+				app: 'origami-registry-ui',
+			},
+			user: {
+				okta_id: oktaId,
+				is_staff: !!oktaId,
+			},
+		});
+		// Send a page view tracking event.
+		oTracking.page();
+		// Track click events on the page.
+		oTracking.click.init();
+		// Track when elements with the attribute `data-o-tracking-view` are visible to the user.
+		oTracking.view.init();
+		// Tell o-tracking to listen for custom events named `oTracking.event`.
+		oTracking.event.init();
+	} else {
+		console.log('Skipping analytics - not a production site');
+	}
 }
+
+
+
+
+
+
